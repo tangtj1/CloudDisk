1ebdd28fe73049343a45f9c621b06744b62b756d
tang <762550778@qq.com>
2018-05-14 17:11:11 +0800

    添加user相关类

---
+++ src/main/java/cn/tangtj/clouddisk/dao/UserDao.java
@@ -0,0 +1,10 @@
+package cn.tangtj.clouddisk.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author tang
+ */
+@Repository
+public class UserDao {
+}
---
+++ src/main/java/cn/tangtj/clouddisk/service/UserService.java
@@ -0,0 +1,10 @@
+package cn.tangtj.clouddisk.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author tang
+ */
+@Service
+public class UserService {
+}
